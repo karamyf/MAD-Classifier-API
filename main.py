25190a5cde90e574294fb13e24a11ab27b2eda4d
karamyf <karam_youssef@emsi-edu.ma>
2024-03-10 00:11:32 +0100

    This is not a commit

---
+++ main.py
@@ -0,0 +1,58 @@
+from fastapi import FastAPI, File, UploadFile
+from tensorflow.keras.preprocessing.image import load_img, img_to_array
+from tensorflow.keras.models import load_model
+import numpy as np
+import io
+
+# Load the saved model
+model_path = "mad_classifier_model.h5"
+model = load_model(model_path)
+
+# Define the FastAPI app
+app = FastAPI()
+
+# Function to preprocess an image
+def preprocess_image(file):
+    # Read the file as bytes
+    image_bytes = file.read()
+
+    # Load the image
+    img = load_img(io.BytesIO(image_bytes), target_size=(224, 224))  # Adjust target_size if needed
+    img = img_to_array(img)
+    img = img / 255.0  # Normalize pixel values to [0, 1]
+    img = np.expand_dims(img, axis=0)  # Add batch dimension
+    return img
+
+# Endpoint to classify an uploaded image
+@app.post("/classify/")
+async def classify_image(file: UploadFile = File(...)):
+    # Preprocess the image
+    img = preprocess_image(file.file)
+    
+    # Make predictions
+    predictions = model.predict(img)
+    predicted_class = np.argmax(predictions, axis=1)[0]  # Get the predicted class index
+    
+    # Define the class labels mapping
+    class_labels = {
+    0: '0.1 MAD',
+    1: '0.2 MAD',
+    2: '0.5 MAD',
+    3: '1 MAD',
+    4: '10 MAD',
+    5: '100 MAD',
+    6: '2 MAD',
+    7: '20 MAD',
+    8: '200 MAD',
+    9: '5 MAD',
+    10: '50 MAD'
+}
+
+
+    # Get the predicted class label
+    predicted_label = class_labels.get(predicted_class, "Unknown")
+
+    # Get the model's accuracy
+    accuracy = float(predictions[0][predicted_class])
+
+    return {"predicted_class": predicted_label, "accuracy": accuracy}
